8080a3206b030f8d8364da24297b5b77c47f2b3f
feizheng <1290657123@qq.com>
2016-09-23 00:12:05 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-console.log(__dirname);
-
-module.exports = {
+require('next-js-core2')(require);
 
 
-
-};
+module.exports = nx.declare({
+  statics: {
+    get_project_name: function(arguments) {
+      return path.basename(process.cwd());
+    },
+    rewrite_props: function(inProps) {
+      var props = this.props;
+      nx.each(inProps, function(prop, key) {
+        inProps[this.camelcase(key)] = this.camelcase(prop);
+      }, this);
+    },
+    camelcase: function(inString) {
+      return inString.replace(/[_-]\D/g, function(match) {
+        return match.charAt(1).toUpperCase();
+      });
+    }
+  }
+});

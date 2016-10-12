748920fd82a607b9f794147d81d6357fdcacc384
feizheng <1290657123@qq.com>
2016-10-12 12:59:42 +0800

    bugfix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var path = require('path');
-require('next-js-core2')(require);
+var nx = require('next-js-core2');
 
 module.exports = nx.declare({
   statics: {
-    get_project_name: function(arguments) {
+    discoverRoot: function(arguments) {
       return path.basename(process.cwd());
     },
-    rewrite_props: function(inProps) {
+    rewriteProps: function(inProps) {
       var props = this.props;
       nx.each(inProps, function(key, prop) {
         inProps[nx.camelCase(key)] = nx.camelCase(prop);

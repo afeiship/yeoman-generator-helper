5dfa07ea6407ce58333c4763dba15fb94ff448c0
afeiship <1290657123@qq.com>
2020-03-27 14:58:48 +0800

    fix: yeoman rewrite props bug

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ require('@feizheng/next-underscored');
 
 module.exports = nx.declare({
   statics: {
-    discoverRoot: function() {
+    discoverRoot: function () {
       return path.basename(process.cwd());
     },
-    shortName: function(inName) {
+    shortName: function (inName) {
       var name = inName || this.discoverRoot();
       var paths = name.split('-');
       paths.shift();
       return paths.join('-');
     },
-    underToDot: function(inContext) {
+    underToDot: function (inContext) {
       this.rename(inContext, /_/, '.');
     },
-    rewriteProps: function(inProps, inOptions) {
+    rewriteProps: function (inProps, inOptions) {
       var options = nx.mix(DEFAULT_OPTIONS, inOptions);
       nx.each(
         inProps,
-        function(key, prop) {
+        function (key, prop) {
           if (options.exclude.indexOf(key) === -1) {
             //camelCase
             inProps[nx.camelize(key)] = nx.camelize(prop);
@@ -34,15 +34,17 @@ module.exports = nx.declare({
             inProps[nx.classify(key)] = nx.classify(prop);
             //UNDERSCORED_CASE
             inProps[nx.underscored(key).toUpperCase()] = nx.underscored(prop).toUpperCase();
+            // revert like `prefix- ` prop
+            inProps[key] = prop;
           }
         },
         this
       );
     },
-    renameBy: function(inSelf, inCallback) {
+    renameBy: function (inSelf, inCallback) {
       inSelf.registerTransformStream(rename(inCallback));
     },
-    rename: function(inSelf, inSrc, inTarget) {
+    rename: function (inSelf, inSrc, inTarget) {
       inSelf.registerTransformStream(
         rename((path) => {
           path.basename = path.basename.replace(inSrc, inTarget);
